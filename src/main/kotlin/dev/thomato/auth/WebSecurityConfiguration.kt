64d0b35cd62313b1e946bae6966ba29be9e0fda7
Nando Thomassen <nando@thomassen.pro>
2025-08-21 16:14:39 +0200

    🔒 Configure security to allow GraphQL access
    Disables CSRF for GraphQL as we don't work with sessions and
    everything is a POST-request in GraphQL.
    
    It further allows all access to GraphQL.
    
    Note: This should probably be changed in the future, but as long as
    there's no working login, this allows testing.

---
+++ src/main/kotlin/dev/thomato/auth/WebSecurityConfiguration.kt
@@ -0,0 +1,24 @@
+package dev.thomato.auth
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests {
+            it.requestMatchers("/graphiql", "/graphql").permitAll()
+                .anyRequest().authenticated()
+        }
+            .csrf { csrf ->
+                csrf.ignoringRequestMatchers("/graphql/**")  // Disable CSRF for GraphQL
+            }
+
+        return http.build()
+    }
+}
