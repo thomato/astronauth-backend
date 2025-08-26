385d2e93d8fb5e30051320ad2b69ef24b663fe46
Nando Thomassen <nando@thomassen.pro>
2025-08-26 15:41:52 +0200

    feat(registration): add user persistence
    Save new users

---
+++ src/main/kotlin/dev/thomato/auth/user/UserRepository.kt
@@ -0,0 +1,8 @@
+package dev.thomato.auth.user
+
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface UserRepository : CoroutineCrudRepository<User, UUID>
--- src/main/kotlin/dev/thomato/auth/user/registration/UserRegistrationController.kt
+++ src/main/kotlin/dev/thomato/auth/user/registration/UserRegistrationController.kt
@@ -1,13 +1,27 @@
 package dev.thomato.auth.user.registration
 
+import dev.thomato.auth.user.User
+import dev.thomato.auth.user.UserRepository
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.stereotype.Controller
 
 @Controller
-class UserRegistrationController {
+class UserRegistrationController(
+    private val userRepository: UserRepository
+) {
     @MutationMapping
-    fun registerUser(@Argument input: RegisterUserInput): String {
-        return "successish"
+    suspend fun registerUser(@Argument input: RegisterUserInput): String {
+        if (input.password != input.confirmPassword) {
+            return "Passwords do not match"
+        }
+        
+        val user = User(
+            email = input.email,
+            password = input.password
+        )
+        
+        val savedUser = userRepository.save(user)
+        return "User registered successfully with ID: ${savedUser.id}"
     }
 }
